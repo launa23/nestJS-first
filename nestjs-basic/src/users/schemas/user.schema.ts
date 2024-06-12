b36fbd64c280d1803739acc7b2920db1f2776ee2
Laun <luan2003hhbg@gmail.com>
2024-06-12 14:51:59 +0700

    Update

--- nestjs-basic/src/users/schemas/user.schema.ts
+++ nestjs-basic/src/users/schemas/user.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { hashSync } from 'bcryptjs';
+import { NextFunction } from 'express';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
@@ -9,10 +11,10 @@ export class User {
   @Prop()
   name: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   email: string;
 
-  @Prop({required: true})
+  @Prop({ required: true })
   password: string;
 
   @Prop()
