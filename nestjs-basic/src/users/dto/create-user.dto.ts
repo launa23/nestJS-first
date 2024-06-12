62c065f8b7943cb1a22c0d70cea0bfee7cf56a69
Laun <luan2003hhbg@gmail.com>
2024-06-12 13:48:05 +0700

    Update

--- nestjs-basic/src/users/dto/create-user.dto.ts
+++ nestjs-basic/src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail({}, {message: "Email khong dung dinh dang"})
+    @IsNotEmpty({message: "Khong duoc de trong email"})
+    email: string;
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsNumber()
+    age: number;
+}
--- nestjs-basic/src/users/users.service.ts
+++ nestjs-basic/src/users/users.service.ts
@@ -4,12 +4,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import {genSaltSync, hashSync} from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) 
+    private userModel: Model<User>
+  ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  hashPassword = ( password:string ) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  }
+
+  async create(createUserDTO : CreateUserDto) {
+    const hashPassword = this.hashPassword(createUserDTO.password);
+    createUserDTO.password = hashPassword;
+    let user = await this.userModel.create(createUserDTO);
+    return user;
   }
 
   findAll() {
